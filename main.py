c78627c0d19afab38ca31163b4a8519e7c6e17dd
user36672 <anton.ingrish@gmail.com>
2023-02-09 17:38:20 +0300

    create app window

--- main.py
+++ main.py
@@ -1,2 +1,16 @@
-if __name__ == "__main__":
-    pass
+from ui import Ui_MainWindow
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow
+
+
+class MainWindow(QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        super(MainWindow, self).__init__()
+        super().setupUi(self)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec())
