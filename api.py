5f1abc9830a819d9358cdbadbe2d0ac00fb2c8a0
TimurBazarov <bazarov.080706@yandex.ru>
2023-02-09 18:44:14 +0300

    AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA BASED

--- api.py
+++ api.py
@@ -3,6 +3,7 @@ import io
 import requests
 from PIL import Image
 from enum import Enum
+from PIL.ImageQt import ImageQt
 
 URL = f"https://static-maps.yandex.ru/1.x"
 
@@ -15,9 +16,10 @@ class Scheme(Enum):
 
 # Пример использования:
 # get_map(-25.694422, 133.791467, (25, 35), scheme=Scheme.Map)
-def get_map(latitude, longitude, size, scheme=Scheme.Hybrid):
-    url = f"{URL}/?ll={longitude},{latitude}&spn={size[0]},{size[1]}&l={scheme.value}"
+def get_map(latitude, longitude, scheme=Scheme.Hybrid):
+    url = f"{URL}/?ll={longitude},{latitude}&size=650,450&l={scheme.value}&spn=25,35"
+    print(url)
     if response := requests.get(url):
-        return Image.open(io.BytesIO(response.content))
+        return ImageQt(Image.open(io.BytesIO(response.content)))
 
     raise requests.ConnectionError
--- main.py
+++ main.py
@@ -1,12 +1,24 @@
 from ui import Ui_MainWindow
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel
+from PyQt5.QtGui import QPixmap
+
+
+from api import Scheme, get_map
 
 
 class MainWindow(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
         super().setupUi(self)
+        height_offset = self.clear_serch.height() + self.hybrid.height()
+        print((self.width(), self.height() - height_offset))
+        self.pixmap = QPixmap.fromImage(get_map(-25.694422, 133.791467,
+                                                scheme=Scheme.Map))
+
+        self.map.move(0, height_offset)
+        self.map.setPixmap(self.pixmap)
+        self.resize(self.map.width(), self.map.height() + height_offset)
 
 
 if __name__ == '__main__':
